23997fef9ada06135048557cc6b1104899e4d28a
Simon Thiboutôt <simonthiboutot@hotmail.com>
2017-10-23 01:01:39 -0400

    Test against JSON schemas.

--- spec/ultimateGuitarScraperSpec.js
+++ spec/ultimateGuitarScraperSpec.js
@@ -1,8 +1,33 @@
 /* eslint-env jasmine */
 const utils = require('../lib/utils')
 const ugs = require('../lib/index')
+const Ajv = require('ajv')
+const ajv = new Ajv()
 
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000
+
+// TODO: Move this in helper file?
+const jsonSchemaMatcher = {
+  toMatchJsonSchema: (util, customEqualityTesters) => {
+    return {
+      compare: (actual, expected) => {
+        const schema = require(`./support/schemas/${expected}.json`)
+        const valid = ajv.validate(schema, actual)
+
+        if (!valid) console.error(ajv.errors)
+
+        const result = {}
+        result.pass = valid === true
+        if (result.pass) {
+          result.message = 'Expected ' + JSON.stringify(actual, null, 2) + `not to match schema ${expected}.json`
+        } else {
+          result.message = 'Expected ' + JSON.stringify(actual, null, 2) + `to match schema ${expected}.json`
+        }
+        return result
+      }
+    }
+  }
+}
 
 function basicSearchQuery () {
   return {
@@ -99,13 +124,19 @@ describe('utils', () => {
 })
 
 describe('ultimate-guitar-scraper', () => {
+  beforeEach(() => {
+    jasmine.addMatchers(jsonSchemaMatcher)
+  })
+
   describe('search', () => {
     it('searches TABs', (done) => {
       let query = basicSearchQuery()
-      ugs.search(query, (error, results) => {
+      ugs.search(query, (error, tabs) => {
         expect(error).toBeNull()
-        expect(Array.isArray(results)).toBe(true)
-        expect(results.length).toBeGreaterThan(0)
+
+        expect(tabs.length).toBeGreaterThan(0)
+        expect(tabs).toMatchJsonSchema('tabs')
+
         done()
       })
     })
@@ -115,10 +146,11 @@ describe('ultimate-guitar-scraper', () => {
       let requestOptions = {
         headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36' }
       }
-      ugs.search(query, (error, results, response, body) => {
+      ugs.search(query, (error, tabs, response, body) => {
         expect(error).toBeNull()
-        expect(Array.isArray(results)).toBe(true)
-        expect(results.length).toBeGreaterThan(0)
+
+        expect(tabs.length).toBeGreaterThan(0)
+        expect(tabs).toMatchJsonSchema('tabs')
 
         expect(response.statusCode).toBe(200)
         expect(typeof body).toBe('string')
@@ -137,21 +169,8 @@ describe('ultimate-guitar-scraper', () => {
     it('get the TAB', (done) => {
       ugs.get(tabUrl, (error, tab) => {
         expect(error).toBeNull()
-        expect(typeof tab).toBe('object')
-        expect(typeof tab.name).toBe('string')
-        expect(typeof tab.type).toBe('string')
-        expect(typeof tab.artist).toBe('string')
-        expect(typeof tab.artist).toBe('string')
+        expect(tab).toMatchJsonSchema('tab')
 
-        expect(typeof tab.contentText).toBe('string')
-        expect(tab.contentText.trim().length).toBeGreaterThan(0)
-
-        expect(typeof tab.contentHTML).toBe('string')
-        expect(tab.contentHTML.trim().length).toBeGreaterThan(0)
-        // Optional properties.
-        expect(typeof tab.rating).not.toBe('undefined')
-        expect(typeof tab.numberRates).not.toBe('undefined')
-        expect(typeof tab.difficulty).not.toBe('undefined')
         done()
       })
     })
@@ -162,17 +181,11 @@ describe('ultimate-guitar-scraper', () => {
       }
       ugs.get(tabUrl, (error, tab, response, body) => {
         expect(error).toBeNull()
-        expect(typeof tab).toBe('object')
-        expect(typeof tab.name).toBe('string')
-        expect(typeof tab.type).toBe('string')
-        expect(typeof tab.artist).toBe('string')
-        // Optional properties.
-        expect(typeof tab.rating).not.toBe('undefined')
-        expect(typeof tab.numberRates).not.toBe('undefined')
-        expect(typeof tab.difficulty).not.toBe('undefined')
+        expect(tab).toMatchJsonSchema('tab')
 
         expect(response.statusCode).toBe(200)
         expect(typeof body).toBe('string')
+
         done()
       }, requestOptions)
     })
@@ -183,8 +196,8 @@ describe('ultimate-guitar-scraper', () => {
       let query = 'Ozzy'
       ugs.autocomplete(query, (error, suggestions) => {
         expect(error).toBeNull()
-        expect(Array.isArray(suggestions)).toBe(true)
         expect(suggestions.length).toBeGreaterThan(0)
+        expect(suggestions).toMatchJsonSchema('suggestions')
         done()
       })
     })
@@ -196,8 +209,9 @@ describe('ultimate-guitar-scraper', () => {
       let query = 'Crazy'
       ugs.autocomplete(query, (error, suggestions, response, body) => {
         expect(error).toBeNull()
-        expect(Array.isArray(suggestions)).toBe(true)
+
         expect(suggestions.length).toBeGreaterThan(0)
+        expect(suggestions).toMatchJsonSchema('suggestions')
 
         expect(response.statusCode).toBe(200)
         expect(typeof body).toBe('string')
