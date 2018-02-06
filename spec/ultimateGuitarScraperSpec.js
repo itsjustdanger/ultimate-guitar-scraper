e9b72725a6aec10d761a9fd3c04d7e3e1203b121
Simon Thiboutôt <simonthiboutot@hotmail.com>
2018-02-06 07:54:09 -0500

    Fix search.
    Remove advanced search (no longer supported by ultimate guitar).

--- spec/ultimateGuitarScraperSpec.js
+++ spec/ultimateGuitarScraperSpec.js
@@ -24,12 +24,6 @@ function basicSearchQuery () {
   }
 }
 
-function basicAdvanceSearchQuery () {
-  return {
-    bandName: 'Muse'
-  }
-}
-
 function autocompleteQuery () {
   return 'Ozzy'
 }
@@ -42,14 +36,6 @@ function completeSearchQuery () {
   }
 }
 
-function completeAdvanceSearchQuery () {
-  return {
-    bandName: 'Black Keys',
-    songName: 'Little Black Submarines',
-    type: ['video lessons', 'tabs', 'chords', 'bass tabs', 'guitar pro tabs', 'power tabs', 'drum tabs', 'ukulele chords'],
-    page: 1
-  }
-}
 
 describe('ultimate-guitar-scraper', () => {
   describe('search', () => {
@@ -71,38 +57,6 @@ describe('ultimate-guitar-scraper', () => {
         headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36' }
       }
       ugs.search(query, (error, tabs, response, body) => {
-        expect(error).toBeNull()
-
-        expect(tabs.length).toBeGreaterThan(0)
-        expect(tabs).toMatchJsonSchema('tabs')
-
-        expect(response.statusCode).toBe(200)
-        expect(typeof body).toBe('string')
-
-        done()
-      }, requestOptions)
-    })
-  })
-
-  describe('advanceSearch', () => {
-    it('searches TABs', (done) => {
-      let query = basicAdvanceSearchQuery()
-      ugs.advanceSearch(query, (error, tabs) => {
-        expect(error).toBeNull()
-
-        expect(tabs.length).toBeGreaterThan(0)
-        expect(tabs).toMatchJsonSchema('tabs')
-
-        done()
-      })
-    })
-
-    it('searches TABs with request options', (done) => {
-      let query = completeAdvanceSearchQuery()
-      let requestOptions = {
-        headers: { 'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.86 Safari/537.36' }
-      }
-      ugs.advanceSearch(query, (error, tabs, response, body) => {
         expect(error).toBeNull()
 
         expect(tabs.length).toBeGreaterThan(0)
--- spec/utilsSpec.js
+++ spec/utilsSpec.js
@@ -40,49 +40,50 @@ describe('utils', () => {
     })
   })
 
-  describe('formatAdvanceSearchQuery', () => {
-    it('is invalid without param bandName', () => {
-      expect(() => {
-        utils.formatAdvanceSearchQuery({})
-      }).toThrowError(Error)
-    })
+  // describe('formatAdvanceSearchQuery', () => {
+  //   it('is invalid without param bandName', () => {
+  //     expect(() => {
+  //       utils.formatAdvanceSearchQuery({})
+  //     }).toThrowError(Error)
+  //   })
+  //
+  //   it('uses default params', () => {
+  //     let query = {
+  //       bandName: 'Muse'
+  //     }
+  //
+  //     expect(utils.formatAdvanceSearchQuery(query)).toEqual({
+  //       band_name: 'Muse',
+  //       type: [ 300, 200 ],
+  //       page: 1,
+  //       view_state: 'advanced',
+  //       tab_type_group: 'text',
+  //       app_name: 'ugt',
+  //       order: 'myweight',
+  //       version_la: ''
+  //     })
+  //   })
+  //
+  //   it('uses params', () => {
+  //     let query = {
+  //       bandName: 'Black Keys',
+  //       songName: 'Little Black Submarines',
+  //       type: ['video lessons', 'tabs', 'chords', 'bass tabs', 'guitar pro tabs', 'power tabs', 'drum tabs', 'ukulele chords'],
+  //       page: 1
+  //     }
+  //
+  //     expect(utils.formatAdvanceSearchQuery(query)).toEqual({
+  //       band_name: 'Black Keys',
+  //       song_name: 'Little Black Submarines',
+  //       type: [ 100, 200, 300, 400, 500, 600, 700, 800 ],
+  //       page: 1,
+  //       view_state: 'advanced',
+  //       tab_type_group: 'text',
+  //       app_name: 'ugt',
+  //       order: 'myweight',
+  //       version_la: ''
+  //     })
+  //   })
+  // })
 
-    it('uses default params', () => {
-      let query = {
-        bandName: 'Muse'
-      }
-
-      expect(utils.formatAdvanceSearchQuery(query)).toEqual({
-        band_name: 'Muse',
-        type: [ 300, 200 ],
-        page: 1,
-        view_state: 'advanced',
-        tab_type_group: 'text',
-        app_name: 'ugt',
-        order: 'myweight',
-        version_la: ''
-      })
-    })
-
-    it('uses params', () => {
-      let query = {
-        bandName: 'Black Keys',
-        songName: 'Little Black Submarines',
-        type: ['video lessons', 'tabs', 'chords', 'bass tabs', 'guitar pro tabs', 'power tabs', 'drum tabs', 'ukulele chords'],
-        page: 1
-      }
-
-      expect(utils.formatAdvanceSearchQuery(query)).toEqual({
-        band_name: 'Black Keys',
-        song_name: 'Little Black Submarines',
-        type: [ 100, 200, 300, 400, 500, 600, 700, 800 ],
-        page: 1,
-        view_state: 'advanced',
-        tab_type_group: 'text',
-        app_name: 'ugt',
-        order: 'myweight',
-        version_la: ''
-      })
-    })
-  })
 })
